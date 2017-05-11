37690ca47865871ad25fa02fc486a87b060ded6d
Gueckmooh <gueckmooh@gmail.com>
2017-05-11 14:41:37 +0200

    Ajout de la detection de la case pointée

---
+++ src/Vue/Pointeur.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vue;
+
+import Modele.Case;
+import Modele.Etendeur;
+import Modele.Plateau;
+import Modele.Point;
+import Modele.Visiteur;
+import static Vue.Dessinateur.c;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
+
+/**
+ *
+ * @author brignone
+ */
+public class Pointeur extends Visiteur {
+    static Canvas c;
+    Etendeur etendeur;
+    MouseEvent me;
+    
+    public Pointeur (Canvas c, MouseEvent me) {
+        this.c = c;
+        this.me = me;
+        etendeur = new Etendeur();
+    }
+    
+    @Override
+    public boolean visite (Plateau p) {
+        etendeur.fixeEchelle(c, p);
+        return false;
+    }
+    
+    @Override
+    public boolean visite (Case c) {
+        boolean b = true;
+        double x1, x2, y1, y2;
+        etendeur.fixeComposant(c);
+        double [][] coords = Interface.hex_corner(etendeur.x(), etendeur.y(), etendeur.h()/2);
+        //System.out.println("\nCase coord : [" + c.position().x() + ";" + c.position().y() + "]");
+        //System.out.println("Souris : [" + me.getX() + ";" + me.getY() + "]");
+        //for (int i = 0; i < 6; i++) {
+            //System.out.println("[" + coords[0][i] + ";" + coords[1][i] + "]");
+        //}
+        for (int i = 0; i < 6; i++) {
+            x1 = coords[0][(i+1)%6] - coords[0][i];
+            y1 = coords[1][(i+1)%6] - coords[1][i];
+
+            x2 = me.getX() - coords[0][i];
+            y2 = me.getY() - coords[1][i];
+            
+            b = b&&((x1*y2) - (x2*y1))>0;
+            //System.out.println((((x1*y2) - (x2*y1))>0));
+        }
+        if (b)
+            System.out.println("[" + c.position().x()+ ";" + c.position().y()+ "]");
+        return false;
+    }
+}
